09471eb4fd6ded7417c27d541a0c4272162adf4e
Houre Souren <112374244+houresouren81@users.noreply.github.com>
2023-09-12 11:56:38 +0200

    Add project files.

---
+++ flytta knapp/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace flytta_knapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.tbxX = new System.Windows.Forms.TextBox();
+            this.tbxY = new System.Windows.Forms.TextBox();
+            this.btnFlytta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(93, 47);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(26, 25);
+            this.lblX.TabIndex = 0;
+            this.lblX.Text = "X";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(98, 110);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(25, 25);
+            this.lblY.TabIndex = 1;
+            this.lblY.Text = "Y";
+            // 
+            // tbxX
+            // 
+            this.tbxX.Location = new System.Drawing.Point(293, 47);
+            this.tbxX.Name = "tbxX";
+            this.tbxX.Size = new System.Drawing.Size(207, 29);
+            this.tbxX.TabIndex = 2;
+            // 
+            // tbxY
+            // 
+            this.tbxY.Location = new System.Drawing.Point(293, 110);
+            this.tbxY.Name = "tbxY";
+            this.tbxY.Size = new System.Drawing.Size(207, 29);
+            this.tbxY.TabIndex = 3;
+            // 
+            // btnFlytta
+            // 
+            this.btnFlytta.Location = new System.Drawing.Point(266, 276);
+            this.btnFlytta.Name = "btnFlytta";
+            this.btnFlytta.Size = new System.Drawing.Size(248, 98);
+            this.btnFlytta.TabIndex = 4;
+            this.btnFlytta.Text = "Flytta";
+            this.btnFlytta.UseVisualStyleBackColor = true;
+            this.btnFlytta.Click += new System.EventHandler(this.btnFlytta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFlytta);
+            this.Controls.Add(this.tbxY);
+            this.Controls.Add(this.tbxX);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Name = "Form1";
+            this.Text = "flytta knapp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.TextBox tbxX;
+        private System.Windows.Forms.TextBox tbxY;
+        private System.Windows.Forms.Button btnFlytta;
+    }
+}
+
---
+++ flytta knapp/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flytta_knapp
+{
+    public partial class Form1 : Form
+    {
+        int X, Y;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFlytta_Click(object sender, EventArgs e)
+        {
+
+            X= Convert.ToInt16(tbxX.Text);
+            Y = Convert.ToInt16(tbxY.Text);
+
+            Point nyPosition = new Point(X, Y);
+
+            
+            btnFlytta.Location = nyPosition;
+        }
+    }
+}
